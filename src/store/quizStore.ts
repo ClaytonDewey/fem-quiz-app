f83325b66b10a0e7449d8af124eee704a4b758fa
ClaytonDewey <clay@dryadmedia.com>
2025-10-15 13:57:14 -0500

    refactor: Add questions/answers

--- src/store/quizStore.ts
+++ src/store/quizStore.ts
@@ -2,12 +2,16 @@ import { create } from 'zustand';
 
 type State = {
   topic: string;
+  questions: string[];
+  answers: string[];
   score: number;
   activeQuestionIndex: number;
 };
 
 type Action = {
   updateTopic: (topic: State['topic']) => void;
+  updateQuestions: (questions: State['questions']) => void;
+  updateAnswers: (answers: State['answers']) => void;
   updateScore: (score: State['score']) => void;
   updateActiveQuestionIndex: (
     activeQuestionIndex: State['activeQuestionIndex']
@@ -16,9 +20,13 @@ type Action = {
 
 export const useQuizStore = create<State & Action>((set) => ({
   topic: '',
+  questions: [],
+  answers: [],
   score: 0,
   activeQuestionIndex: 0,
   updateTopic: (topic) => set(() => ({ topic: topic })),
+  updateQuestions: (questions) => () => ({ questions: questions }),
+  updateAnswers: (answers) => () => ({ answers: answers }),
   updateScore: (score) => set(() => ({ score: score })),
   updateActiveQuestionIndex: (activeQuestionIndex) => ({
     activeQuestionIndex: activeQuestionIndex,
